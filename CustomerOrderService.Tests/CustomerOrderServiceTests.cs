d8f2451eba042ecd900412eaf8db6699df167b30
rajunarashima1976 <65461869+rajunarashima1976@users.noreply.github.com>
2020-07-06 13:59:18 +0000

    XXXXXXXXXXX
    XXXX

--- CustomerOrderService.Tests/CustomerOrderServiceTests.cs
+++ CustomerOrderService.Tests/CustomerOrderServiceTests.cs
@@ -39,7 +39,8 @@ namespace CustomerOrderService.Tests
 
 
 
-           Assert.AreEqual(order.Amount, 135);
+           //Assert.AreEqual(order.Amount, 135);
+           Assert.AreEqual(order.Amount, 130);
 
 
 
