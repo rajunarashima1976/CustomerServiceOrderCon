a100902d796dea76e72b23eb3e5677b6d52651a1
rajunarashima1976 <65461869+rajunarashima1976@users.noreply.github.com>
2020-07-07 06:42:45 +0000

    MVVVVMBranch new customer order service (#48)
    * Update CustomerOrderServiceTests.cs
    
    * AAA
    
    AAA
    
    * BBBBB
    
    BBBBBBB
    
    * CCCCCCCCC
    
    CCCCC
    
    * MMMMMMMMMM
    
    MMMMM
    
    * PPPPPPPPPP
    
    PPPP
    
    * XXXXXXXXXXX
    
    XXXX
    
    * kkkkkkkkkkkkkkkkk
    
    kkkkk
    
    * ZZZZZZZZZZ
    
    ZZZ
    
    * cccc
    
    cccccc
    
    * NNNNNNNNNN
    
    NNN
    
    * code chnages in branch
    
    codecode
    
    * MAAAAAAA
    
    MA
    
    * MASSSSSSSS
    
    MAS
    
    * NewChanges
    
    * NewCommets
    
    asdasd
    
    * sddasdasd
    
    asdasdadasd
    
    * Added New1
    
    New1
    
    * LLLLL
    
    PQAAAA
    
    * LAAAAAAAA
    
    LAAA
    
    * testcase changes
    
    testcasechanges
    
    * Commited Newly
    
    NewlyNewly
    
    * MMMMMerage
    
    Mergeaddednewlyto
    
    * NASSSSSS
    
    NAS
    
    * Newlycommited
    
    comittedadded
    
    * NSSSSS
    
    NSS addded
    
    * NewlyAdded
    
    Added
    
    * Commiting a new chnages
    
    new chnages
    
    * Only Test Case
    
    Test Case
    
    * Update CustomerOrderServiceTests.cs
    
    * MMMAD
    
    MAD
    
    * dfsdfs
    
    dfsdfsdf
    
    * ner
    
    ner
    
    * KITTTTTTTT
    
    KIT
    
    * Addddddddd
    
    Add
    
    * MBNVVV
    
    MBVVVV
    
    * MVVVVVVVVM
    
    MBBBBBVM

--- CustomerOrderService.Tests/CustomerOrderServiceTests.cs
+++ CustomerOrderService.Tests/CustomerOrderServiceTests.cs
@@ -35,16 +35,13 @@ namespace CustomerOrderService.Tests
            //Assert
 
            //Assert.AreEqual(order.Amount, 135);
-            Assert.AreEqual(order.Amount, 130);
-
-          //Assert.AreEqual(order.Amount, 135);
+           
         
 
 
 
-
-
            Assert.AreEqual(order.Amount, 135);
+
 
 
 
