57ddd7f1b11d9817e1f2fa607bee2106444acb0c
rajunarashima1976 <65461869+rajunarashima1976@users.noreply.github.com>
2020-07-07 08:50:53 +0000

    sdfsdf
    sdfsdf

--- CustomerOrderService/Customer.cs
+++ CustomerOrderService/Customer.cs
@@ -6,26 +6,7 @@ namespace CustomerOrderService
 {
     public class Customer
     {
-        public int CustomerId { get; set; }
-        public string CustomerName { get; set; }
-        public string CustomerAddress { get; set; }
-        public string CustAdd1 { get; set; }
-        public string custadd2 { get; set; }
-        public string custadd3 { get; set; }
-        public string custadd4 { get; set; }
-        public string custadd5 { get; set; }
-        public string custadd6 { get; set; }
-
-        public string custadd8 { get; set; }
-
-        public string custadd7 { get; set; }
         
-        public string custadd9 { get; set; }
-        public string custadd10 { get; set; }
-        public string custadd11 { get; set; }
-       
-
-
-        public CustomerType CustomerType { get; set; }
+        
     }
 }
