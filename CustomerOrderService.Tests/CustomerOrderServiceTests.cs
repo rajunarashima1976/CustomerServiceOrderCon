ace0db753ce39540e28b26805166c67171250118
rajunarashima1976 <65461869+rajunarashima1976@users.noreply.github.com>
2020-07-07 06:01:46 +0000

    CCCCCCBranch new customer order service (#47)
    * Update CustomerOrderServiceTests.cs
    
    * AAA
    
    AAA
    
    * BBBBB
    
    BBBBBBB
    
    * CCCCCCCCC
    
    CCCCC
    
    * MMMMMMMMMM
    
    MMMMM
    
    * PPPPPPPPPP
    
    PPPP
    
    * XXXXXXXXXXX
    
    XXXX
    
    * kkkkkkkkkkkkkkkkk
    
    kkkkk
    
    * ZZZZZZZZZZ
    
    ZZZ
    
    * cccc
    
    cccccc
    
    * NNNNNNNNNN
    
    NNN
    
    * code chnages in branch
    
    codecode
    
    * MAAAAAAA
    
    MA
    
    * MASSSSSSSS
    
    MAS
    
    * NewChanges
    
    * NewCommets
    
    asdasd
    
    * sddasdasd
    
    asdasdadasd
    
    * Added New1
    
    New1
    
    * LLLLL
    
    PQAAAA
    
    * LAAAAAAAA
    
    LAAA
    
    * testcase changes
    
    testcasechanges
    
    * Commited Newly
    
    NewlyNewly
    
    * MMMMMerage
    
    Mergeaddednewlyto
    
    * NASSSSSS
    
    NAS
    
    * Newlycommited
    
    comittedadded
    
    * NSSSSS
    
    NSS addded
    
    * NewlyAdded
    
    Added
    
    * Commiting a new chnages
    
    new chnages
    
    * Only Test Case
    
    Test Case
    
    * Update CustomerOrderServiceTests.cs
    
    * MMMAD
    
    MAD
    
    * dfsdfs
    
    dfsdfsdf
    
    * ner
    
    ner
    
    * KITTTTTTTT
    
    KIT
    
    * Addddddddd
    
    Add
    
    * MBNVVV
    
    MBVVVV

--- CustomerOrderService.Tests/CustomerOrderServiceTests.cs
+++ CustomerOrderService.Tests/CustomerOrderServiceTests.cs
@@ -34,6 +34,8 @@ namespace CustomerOrderService.Tests
 
            //Assert
 
+           //Assert.AreEqual(order.Amount, 135);
+            Assert.AreEqual(order.Amount, 130);
 
           //Assert.AreEqual(order.Amount, 135);
         
@@ -41,21 +43,9 @@ namespace CustomerOrderService.Tests
 
 
 
+
            Assert.AreEqual(order.Amount, 135);
 
-
-
-        
-
-
-
-
-
-
-
-
-
-            
 
 
         }
