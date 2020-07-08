6aaa6d94a0d1085ef141dfd10be676a76d8b577b
rajunarashima1976 <65461869+rajunarashima1976@users.noreply.github.com>
2020-07-08 12:06:58 +0000

    FaileTestCase
    TestCaseFailed

--- CustomerOrderService.Tests/CustomerOrderServiceTests.cs
+++ CustomerOrderService.Tests/CustomerOrderServiceTests.cs
@@ -38,7 +38,8 @@ namespace CustomerOrderService.Tests
 
 
 
-           Assert.AreEqual(order.Amount, 135);
+           //Assert.AreEqual(order.Amount, 135);
+           Assert.AreEqual(order.Amount,130);
            
 
 
