daaa056fc165711e545114911e809375721ca9ab
rajunarashima1976 <65461869+rajunarashima1976@users.noreply.github.com>
2020-07-09 08:33:11 +0000

    Branch Test cases removedNo changes in this test case
    No test

--- CustomerOrderService.Tests/CustomerOrderServiceTests.cs
+++ CustomerOrderService.Tests/CustomerOrderServiceTests.cs
@@ -6,56 +6,7 @@ namespace CustomerOrderService.Tests
     public class CustomerOrderServiceTests
     {
 
-        [TestCase]
-        public void When_PremiumCustomer_Expect_10PercentDiscount()
-        {
-            //Arrange
-            Customer premiumCustomer = new Customer
-            {
-                CustomerId = 1,
-                CustomerName = "George",
-                CustomerType = CustomerType.Premium
-            };
-
-            Order order = new Order
-            {
-                OrderId = 1,
-                ProductId = 212,
-                ProductQuantity = 1,
-                Amount = 150
-               
-            };
-
-            CustomerOrderService customerOrderService = new CustomerOrderService();
-
-            //Act
-            customerOrderService.ApplyDiscount(premiumCustomer, order);
-
-
-           //Assert
-
-
-
-
-
-
-
-
-
-
-           Assert.AreEqual(order.Amount, 135);
-
-
-
-
-
-           
-
-
-
-
-
-
+       
 
 
 
