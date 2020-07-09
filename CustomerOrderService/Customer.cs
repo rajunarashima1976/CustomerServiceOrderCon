4b17ff8a37498fdf112db15044e758b89a8e5754
rajunarashima1976 <65461869+rajunarashima1976@users.noreply.github.com>
2020-07-09 15:00:05 +0000

    fsdfsdf
    sdfsdfsdf

--- CustomerOrderService/Customer.cs
+++ CustomerOrderService/Customer.cs
@@ -16,7 +16,8 @@ namespace CustomerOrderService
 
         public string custaddress3 { get; set; }
         public string custaddress4 { get; set; }
-         public string custaddress5 { get; set; }
+        public string custaddress5 { get; set; }
+        public string custaddress6 { get; set; }
        
 
 
